647e600481be19b93c12ab988f652cac71867639
Stefano Butori <butori@gmail.com>
2021-09-22 15:08:16 -0300

    Create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a>
+          <Image
+            src="/images/Logo.svg"
+            alt="logo"
+            width="238.62"
+            height="25.63"
+          />
+        </a>
+      </Link>
+    </header>
+  );
 }
